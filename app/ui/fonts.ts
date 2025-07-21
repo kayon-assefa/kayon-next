c89b1fbf2cb51acb888e6a39ce33bd9c7d433aa5
kayon-assefa <kayonkau@gmail.com>
2025-07-21 12:09:55 +0300

    finished part 1-5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+// app/ui/fonts.ts
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+// app/ui/fonts.ts
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
